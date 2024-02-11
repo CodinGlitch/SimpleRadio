cfb62c28384f2eb3d14e5bf0678042f8ff8f927e
CodinGlitch <codinglitchdev@gmail.com>
2024-02-11 12:23:41 -0800

    Block properties update

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioBlocks.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioBlocks.java
@@ -4,6 +4,8 @@ import com.codinglitch.simpleradio.core.registry.blocks.RadiosmitherBlock;
 import com.codinglitch.simpleradio.core.registry.blocks.RadioBlock;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 
 import java.util.HashMap;
 
@@ -12,8 +14,12 @@ import static com.codinglitch.simpleradio.CommonSimpleRadio.id;
 public class SimpleRadioBlocks {
     public static final HashMap<ResourceLocation, Block> BLOCKS = new HashMap<>();
 
-    public static Block RADIOSMITHER = register(id("radiosmither"), new RadiosmitherBlock(Block.Properties.of()));
-    public static Block RADIO = register(id("radio"), new RadioBlock(Block.Properties.of()));
+    public static Block RADIOSMITHER = register(id("radiosmither"), new RadiosmitherBlock(
+            Block.Properties.of().strength(3.0F, 6.0F).sound(SoundType.DEEPSLATE)
+    ));
+    public static Block RADIO = register(id("radio"), new RadioBlock(
+            Block.Properties.of().strength(3.0F, 6.0F).sound(SoundType.METAL)
+    ));
 
     private static Block register(ResourceLocation location, Block block) {
         BLOCKS.put(location, block);
