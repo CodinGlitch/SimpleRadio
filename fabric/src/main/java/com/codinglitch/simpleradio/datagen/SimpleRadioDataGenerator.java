355baec8e7dafc41fa81223de87c06a1b54e2a2c
CodinGlitch <codinglitchdev@gmail.com>
2024-02-21 00:56:04 -0800

    Created data generator in Fabric

---
+++ fabric/src/main/java/com/codinglitch/simpleradio/datagen/SimpleRadioDataGenerator.java
@@ -0,0 +1,13 @@
+package com.codinglitch.simpleradio.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleRadioDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+
+        pack.addProvider(SimpleRadioRecipeProvider::new);
+    }
+}
