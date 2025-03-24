bfcf0929b2ae384ceadc60053348fe5e24f0b9ef
CodinGlitch <codinglitchdev@gmail.com>
2025-03-24 03:57:54 -0700

    Transceiver model overriding

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioModels.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioModels.java
@@ -2,70 +2,124 @@ package com.codinglitch.simpleradio.core.registry;
 
 import com.codinglitch.simpleradio.CommonSimpleRadio;
 import com.codinglitch.simpleradio.client.models.ModuleModel;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
-import java.lang.reflect.InvocationTargetException;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class SimpleRadioModels {
-    public static final ArrayList<ModelHolder<BakedModel>> MODELS = new ArrayList<>();
+    public static final List<ModelResourceLocation> MODELS = new ArrayList<>();
+    public static final List<ModelSubstitution<BakedModel>> SUBSTITUTIONS = new ArrayList<>();
 
-    public static ModelHolder<ModuleModel> MODULE = register(new ModelHolder<>(ModuleModel.class,
+    public static final List<ModelOverride> OVERRIDES = new ArrayList<>();
+
+    public static ModelSubstitution<ModuleModel> MODULE = register(new ModelSubstitution<>(ModuleModel::new,
             new ModelResourceLocation(CommonSimpleRadio.ID, "iron_module", "inventory"),
             new ModelResourceLocation(CommonSimpleRadio.ID, "gold_module", "inventory"),
             new ModelResourceLocation(CommonSimpleRadio.ID, "diamond_module", "inventory"),
             new ModelResourceLocation(CommonSimpleRadio.ID, "netherite_module", "inventory")
     ));
 
-    public static <M extends BakedModel> ModelHolder<M> register(ModelHolder<M> model) {
-        MODELS.add((ModelHolder<BakedModel>) model);
+    public static ModelResourceLocation TRANSCEIVER = register(new ModelResourceLocation(CommonSimpleRadio.ID, "transceiver_item", "inventory"));
+    public static ModelResourceLocation TRANSCEIVER_IN_HAND = register(new ModelResourceLocation(CommonSimpleRadio.ID, "transceiver", "inventory"));
+
+    // ----
+
+    private static final ModelOverride TRANSCEIVER_OVERRIDE = register(new ModelOverride(
+            List.of(ItemDisplayContext.NONE),
+            TRANSCEIVER, SimpleRadioItems.TRANSCEIVER
+    ));
+
+    // ----
+
+    public static ModelOverride register(ModelOverride override) {
+        OVERRIDES.add(override);
+        return override;
+    }
+
+    public static ModelResourceLocation register(ModelResourceLocation model) {
+        MODELS.add(model);
+        return model;
+    }
+    public static <M extends BakedModel> ModelSubstitution<M> register(ModelSubstitution<M> model) {
+        SUBSTITUTIONS.add((ModelSubstitution<BakedModel>) model);
         return model;
     }
 
+    public static void onModelsRegister(Consumer<ModelResourceLocation> registry) {
+        for (ModelResourceLocation model : MODELS) {
+            CommonSimpleRadio.info("Adding new model for {}", model);
+            registry.accept(model);
+        }
+    }
+
     public static void onModelsLoad(Map<ResourceLocation, BakedModel> bakedRegistry) {
-        for (ModelHolder<BakedModel> model : MODELS) {
-            for (ModelResourceLocation location : model.locations) {
+        for (ModelSubstitution<BakedModel> substitution : SUBSTITUTIONS) {
+            for (ModelResourceLocation location : substitution.locations) {
                 BakedModel existingModel = bakedRegistry.get(location);
                 if (existingModel == null) {
                     CommonSimpleRadio.warn("Could not find model {}", location);
                 } else {
                     CommonSimpleRadio.info("Replacing model for {}", location);
 
-                    try {
-                        BakedModel newModel = model.bakedModel.getDeclaredConstructor(BakedModel.class).newInstance(existingModel);
-                        
-                        if (newModel instanceof LocationHolder locationHolder)
-                            locationHolder.location = location;
-                        
-                        bakedRegistry.put(location, newModel);
-                    } catch (NoSuchMethodException e) {
-                        throw new RuntimeException("Model "+location+" has not declared an appropriate constructor", e);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Model "+location+" has a declared constructor that is inaccessible", e);
-                    } catch (InvocationTargetException | InstantiationException e) {
-                        throw new RuntimeException(e);
-                    }
+                    BakedModel newModel = substitution.substitutor.apply(existingModel);
+                    if (newModel instanceof LocationHolder locationHolder)
+                        locationHolder.location = location;
+
+                    bakedRegistry.put(location, newModel);
                 }
             }
         }
+    }
+
+    public static BakedModel tryOverride(ItemDisplayContext context, ItemStack stack, @Nullable Level level, @Nullable LivingEntity entity, int id, Function<ModelResourceLocation, BakedModel> retriever) {
+        for (ModelOverride override : OVERRIDES) {
+            if (!stack.is(override.item)) continue;
+
+            boolean isInContext = false;
+            for (ItemDisplayContext overrideContext : override.contexts) {
+                if (overrideContext == context) {
+                    isInContext = true;
+                    break;
+                }
+            }
+            if (!isInContext) continue;
+
+            BakedModel newModel = retriever.apply(override.location);
+
+            ClientLevel clientLevel = level instanceof ClientLevel ? (ClientLevel) level : null;
+            return newModel.getOverrides().resolve(newModel, stack, clientLevel, entity, id);
+        }
+
+        return null;
     }
 
     public static class LocationHolder {
         public ModelResourceLocation location = null;
     }
     
-    public static class ModelHolder<M extends BakedModel> {
-        public final Class<M> bakedModel;
+    public static class ModelSubstitution<M extends BakedModel> {
+        public final Function<BakedModel, M> substitutor;
         public final List<ModelResourceLocation> locations;
 
-        public ModelHolder(Class<M> bakedModel, ModelResourceLocation... locations) {
-            this.bakedModel = bakedModel;
+        public ModelSubstitution(Function<BakedModel, M> substitutor, ModelResourceLocation... locations) {
+            this.substitutor = substitutor;
             this.locations = Arrays.stream(locations).toList();
         }
     }
+
+    public record ModelOverride(List<ItemDisplayContext> contexts, ModelResourceLocation location, Item item) { }
 }
