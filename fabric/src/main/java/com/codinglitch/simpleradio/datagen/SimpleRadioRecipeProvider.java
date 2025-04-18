b991f4bff59ae4c881bfae3c4f80e5a2a0319db7
CodinGlitch <codinglitchdev@gmail.com>
2025-04-18 01:31:32 -0700

    1.20.4 recipe datagen

--- fabric/src/main/java/com/codinglitch/simpleradio/datagen/SimpleRadioRecipeProvider.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/datagen/SimpleRadioRecipeProvider.java
@@ -5,39 +5,51 @@ import com.codinglitch.simpleradio.core.central.ItemHolder;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Recipe;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 public class SimpleRadioRecipeProvider extends FabricRecipeProvider {
 
-    public static final HashMap<FinishedRecipe, ResourceLocation> MAP = new HashMap<>();
+    public static final HashMap<Recipe<?>, ResourceLocation> MAP = new HashMap<>();
 
     public SimpleRadioRecipeProvider(FabricDataOutput output) {
         super(output);
     }
 
-    protected Consumer<FinishedRecipe> withItemConditions(Consumer<FinishedRecipe> exporter, Item item) {
+    protected RecipeOutput withItemConditions(RecipeOutput exporter, Item item) {
         Optional<Map.Entry<ResourceLocation, ItemHolder<Item>>> optional = SimpleRadioItems.ITEMS.entrySet().stream().filter(entry -> entry.getValue().get() == item).findFirst();
         if (optional.isEmpty())
             return exporter;
 
         ResourceLocation location = optional.get().getKey();
-        Consumer<FinishedRecipe> output = withConditions(exporter, FabricLoader.itemsEnabled(location.getPath()));
+        RecipeOutput output = withConditions(exporter, FabricLoader.itemsEnabled(location.getPath()));
 
-        return recipe -> {
-            MAP.put(recipe, location);
-            output.accept(recipe);
+        return new RecipeOutput() {
+
+            @Override
+            public void accept(ResourceLocation resourceLocation, Recipe<?> recipe, @Nullable AdvancementHolder advancementHolder) {
+                MAP.put(recipe, location);
+                output.accept(resourceLocation, recipe, advancementHolder);
+            }
+
+            @Override
+            public Advancement.Builder advancement() {
+                return null;
+            }
         };
     }
 
     @Override
-    public void buildRecipes(Consumer<FinishedRecipe> output) {
+    public void buildRecipes(RecipeOutput output) {
         CommonRecipeProvider.defineRecipes(item -> withItemConditions(output, item));
     }
 }
