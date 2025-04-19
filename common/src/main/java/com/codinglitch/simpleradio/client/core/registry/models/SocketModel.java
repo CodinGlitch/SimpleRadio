96755401142e7c04344c4b8477dfe05350f136d0
CodinGlitch <codinglitchdev@gmail.com>
2025-04-19 04:09:39 -0700

    New socket model

---
+++ common/src/main/java/com/codinglitch/simpleradio/client/core/registry/models/SocketModel.java
@@ -0,0 +1,45 @@
+package com.codinglitch.simpleradio.client.core.registry.models;
+
+import com.codinglitch.simpleradio.CommonSimpleRadio;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+
+public class SocketModel extends Model {
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(CommonSimpleRadio.id("socketmodel"), "main");
+	public static final ResourceLocation TEXTURE_LOCATION = CommonSimpleRadio.id("textures/block/socket.png");
+	private final ModelPart bone;
+	public final ModelPart spool;
+
+	public SocketModel(ModelPart root) {
+		super(RenderType::entitySolid);
+		this.bone = root.getChild("bone");
+		this.spool = this.bone.getChild("spool");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition bone = partdefinition.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(12, 12).addBox(-6.0F, -5.0F, 7.0F, 1.0F, 7.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(12, 12).mirror().addBox(-11.0F, -5.0F, 7.0F, 1.0F, 7.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(8.0F, 24.0F, -8.0F));
+
+		PartDefinition spool = bone.addOrReplaceChild("spool", CubeListBuilder.create().texOffs(14, 0).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 12).addBox(-1.0F, -2.0F, -2.0F, 2.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(1.0F, -3.0F, -3.0F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).mirror().addBox(-2.0F, -3.0F, -3.0F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-8.0F, -4.0F, 8.0F));
+
+		return LayerDefinition.create(meshdefinition, 32, 32);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		bone.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
