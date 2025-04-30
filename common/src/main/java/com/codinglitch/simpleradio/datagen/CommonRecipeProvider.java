6e19782cd86dfa1155f2146ac3d8af5e32e974ac
CodinGlitch <codinglitchdev@gmail.com>
2025-04-29 22:41:03 -0700

    Changed recipes

--- common/src/main/java/com/codinglitch/simpleradio/datagen/CommonRecipeProvider.java
+++ common/src/main/java/com/codinglitch/simpleradio/datagen/CommonRecipeProvider.java
@@ -29,7 +29,7 @@ public class CommonRecipeProvider extends RecipeProvider {
     public static void defineRecipes(Function<Item, RecipeOutput> conditionBuilder) {
         ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, SimpleRadioItems.TRANSCEIVER)
                 .define('I', Items.IRON_INGOT)
-                .define('Q', Items.QUARTZ)
+                .define('Q', Items.AMETHYST_SHARD)
                 .define('C', SimpleRadioItems.COPPER_WIRE)
                 .define('A', SimpleRadioItems.ANTENNA)
                 .define('W', SimpleRadioItems.LISTENER_MODULE)
@@ -176,12 +176,12 @@ public class CommonRecipeProvider extends RecipeProvider {
                 .define('A', SimpleRadioItems.ANTENNA)
                 .define('I', Items.IRON_INGOT)
                 .define('C', SimpleRadioItems.COPPER_WIRE)
-                .define('L', Items.LAPIS_LAZULI)
+                .define('P', Items.ENDER_PEARL)
                 .pattern(" A ")
                 .pattern("CIC")
-                .pattern(" L ")
+                .pattern(" P ")
                 .unlockedBy("has_copper_wire", has(SimpleRadioItems.COPPER_WIRE))
-                .unlockedBy("has_lapis_lazuli", has(Items.LAPIS_LAZULI))
+                .unlockedBy("has_ender_pearl", has(Items.ENDER_PEARL))
                 .save(conditionBuilder.apply(SimpleRadioItems.TRANSMITTING_MODULE));
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, SimpleRadioItems.SPEAKER_MODULE)
