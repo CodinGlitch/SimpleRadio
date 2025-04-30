8a0d28813fa78b0376dd7be65771643ff32544ee
CodinGlitch <codinglitchdev@gmail.com>
2025-04-29 21:28:57 -0700

    Change garbage collection handling for entity-bound routers (often items)

--- common/src/main/java/com/codinglitch/simpleradio/api/central/Auricular.java
+++ common/src/main/java/com/codinglitch/simpleradio/api/central/Auricular.java
@@ -1,18 +1,11 @@
 package com.codinglitch.simpleradio.api.central;
 
-import com.codinglitch.simpleradio.radio.CommonRadioPlugin;
 import com.codinglitch.simpleradio.radio.RadioManager;
-import de.maxhenkel.voicechat.api.VoicechatConnection;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 
 import java.util.UUID;
 
 public interface Auricular {
-    static boolean validate(WorldlyPosition position, Class<?> clazz) {
+    static boolean validateLocation(WorldlyPosition position, Class<?> clazz, UUID reference) {
         return RadioManager.verifyLocationCollection(position, clazz);
-    }
-    static boolean validate(Entity entity, Class<?> clazz) {
-        return RadioManager.verifyEntityCollection(entity, stack -> clazz.isAssignableFrom(stack.getItem().getClass()));
     }
 }
