b766ae7dcbf11bbb2adebdcdb5ea424b3ea5d26b
CodinGlitch <codinglitchdev@gmail.com>
2025-05-03 00:35:39 -0700

    Fixed router criterion

--- common/src/main/java/com/codinglitch/simpleradio/mixin/MixinExplosion.java
+++ common/src/main/java/com/codinglitch/simpleradio/mixin/MixinExplosion.java
@@ -1,12 +1,12 @@
 package com.codinglitch.simpleradio.mixin;
 
+import com.codinglitch.simpleradio.central.WorldlyPosition;
 import com.codinglitch.simpleradio.radio.RadioManager;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,7 +27,7 @@ public class MixinExplosion {
     @Inject(at = @At("HEAD"), method = "finalizeExplosion")
     private void simpleradio$finalizeExplosion(CallbackInfo info) {
         if (this.level instanceof ServerLevel serverLevel) {
-            RadioManager.getInstance().onSoundPlayed(serverLevel, new Vec3(this.x, this.y, this.z), Holder.direct(SoundEvents.GENERIC_EXPLODE), 4, (1.0F + (this.level.random.nextFloat() - this.level.random.nextFloat()) * 0.2F) * 0.7F, 1);
+            RadioManager.getInstance().sendSound(new WorldlyPosition((float) this.x, (float) this.y, (float) this.z, serverLevel), Holder.direct(SoundEvents.GENERIC_EXPLODE), 4, (1.0F + (this.level.random.nextFloat() - this.level.random.nextFloat()) * 0.2F) * 0.7F, 1);
         }
     }
 }
--- common/src/main/java/com/codinglitch/simpleradio/mixin/MixinJukeboxBlockEntity.java
+++ common/src/main/java/com/codinglitch/simpleradio/mixin/MixinJukeboxBlockEntity.java
@@ -1,5 +1,6 @@
 package com.codinglitch.simpleradio.mixin;
 
+import com.codinglitch.simpleradio.central.WorldlyPosition;
 import com.codinglitch.simpleradio.radio.RadioManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -36,9 +37,8 @@ public abstract class MixinJukeboxBlockEntity extends BlockEntity implements Cle
         Item item = this.getFirstItem().getItem();
 
         if (item instanceof RecordItem recordItem && level instanceof ServerLevel serverLevel) {
-            RadioManager.getInstance().onSoundPlayed(
-                    serverLevel,
-                    getBlockPos().getCenter(),
+            RadioManager.getInstance().sendSound(
+                    WorldlyPosition.of(getBlockPos().getCenter().toVector3f(), serverLevel),
                     BuiltInRegistries.SOUND_EVENT.wrapAsHolder(recordItem.getSound()),
                     1, 1, this.getBlockPos().asLong()
             );
@@ -48,9 +48,8 @@ public abstract class MixinJukeboxBlockEntity extends BlockEntity implements Cle
     @Inject(method = "stopPlaying()V", at = @At(value = "TAIL"))
     private void simpleradio$stopPlaying_audioGathering(CallbackInfo ci) {
         if (level instanceof ServerLevel serverLevel) {
-            RadioManager.getInstance().onSoundPlayed(
-                    serverLevel,
-                    getBlockPos().getCenter(),
+            RadioManager.getInstance().sendSound(
+                    WorldlyPosition.of(getBlockPos().getCenter().toVector3f(), serverLevel),
                     BuiltInRegistries.SOUND_EVENT.wrapAsHolder(SoundEvents.EMPTY),
                     0, 1, this.getBlockPos().asLong()
             );
@@ -70,9 +69,8 @@ public abstract class MixinJukeboxBlockEntity extends BlockEntity implements Cle
         if (item instanceof RecordItem recordItem && level instanceof ServerLevel serverLevel) {
             float offset = (tickCount - recordStartedTick) / 20f;
 
-            RadioManager.getInstance().onSoundPlayed(
-                    serverLevel,
-                    getBlockPos().getCenter(),
+            RadioManager.getInstance().sendSound(
+                    WorldlyPosition.of(getBlockPos().getCenter().toVector3f(), serverLevel),
                     BuiltInRegistries.SOUND_EVENT.wrapAsHolder(recordItem.getSound()),
                     1, 1,  offset, this.getBlockPos().asLong()
             );
