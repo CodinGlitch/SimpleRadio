ba8ba3187f94b2c792918f9482c15143a6d06a91
CodinGlitch <codinglitchdev@gmail.com>
2025-05-07 20:20:46 -0700

    Effect stream fix

--- common/src/main/java/com/codinglitch/simpleradio/client/core/central/EffectStream.java
+++ common/src/main/java/com/codinglitch/simpleradio/client/core/central/EffectStream.java
@@ -1,16 +1,14 @@
 package com.codinglitch.simpleradio.client.core.central;
 
-import com.codinglitch.simpleradio.CommonSimpleRadio;
 import com.codinglitch.simpleradio.radio.effects.AudioEffect;
-import com.mojang.blaze3d.audio.OggAudioStream;
-import org.joml.Math;
+import net.minecraft.client.sounds.JOrbisAudioStream;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ShortBuffer;
 
-public class EffectStream extends OggAudioStream {
+public class EffectStream extends JOrbisAudioStream {
     public AudioEffect effect;
 
     public EffectStream(InputStream inputStream) throws IOException {
