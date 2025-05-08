b21a182191d16e4336e344816603ef5949fb23a2
CodinGlitch <codinglitchdev@gmail.com>
2025-05-07 20:36:18 -0700

    1.21.1 networking setup

--- common/src/main/java/com/codinglitch/simpleradio/core/networking/SimpleRadioNetworking.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/networking/SimpleRadioNetworking.java
@@ -1,36 +1,35 @@
 package com.codinglitch.simpleradio.core.networking;
 
 import com.codinglitch.simpleradio.CommonSimpleRadio;
+import com.codinglitch.simpleradio.api.central.Frequency;
 import com.codinglitch.simpleradio.client.core.SimpleRadioClientNetworking;
 import com.codinglitch.simpleradio.core.networking.packets.*;
 import com.codinglitch.simpleradio.core.registry.menus.RadiosmitherMenu;
 import com.codinglitch.simpleradio.platform.Services;
 import com.codinglitch.simpleradio.radio.RadioManager;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import org.apache.logging.log4j.util.TriConsumer;
 
 import java.util.UUID;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class SimpleRadioNetworking {
     public interface ServerboundRegistry {
-        <P extends CustomPacket> void register(
-                ResourceLocation id, Class<P> packetClass,
-                FriendlyByteBuf.Reader<P> reader,
-                BiConsumer<P, FriendlyByteBuf> writer,
+        <P extends CustomPacket, B extends FriendlyByteBuf> void register(
+                CustomPacketPayload.Type<P> type, Class<P> packetClass,
+                StreamCodec<B, P> codec,
                 TriConsumer<P, MinecraftServer, ServerPlayer> handler
         );
     }
     public interface ClientboundRegistry {
-        <P extends CustomPacket> void register(
-                ResourceLocation id, Class<P> packetClass,
-                FriendlyByteBuf.Reader<P> reader,
-                BiConsumer<P, FriendlyByteBuf> writer,
+        <P extends CustomPacket, B extends FriendlyByteBuf> void register(
+                CustomPacketPayload.Type<P> type, Class<P> packetClass,
+                StreamCodec<B, P> codec,
                 Consumer<P> handler
         );
     }
@@ -38,22 +37,22 @@ public class SimpleRadioNetworking {
     // ---- Packets ---- \\
 
     public static void loadServerbound(ServerboundRegistry registry) {
-        registry.register(ServerboundRadioUpdatePacket.ID, ServerboundRadioUpdatePacket.class, ServerboundRadioUpdatePacket::read, ServerboundRadioUpdatePacket::write, SimpleRadioNetworking::handleRadioUpdate);
-        registry.register(ServerboundRequestRouterPacket.ID, ServerboundRequestRouterPacket.class, ServerboundRequestRouterPacket::read, ServerboundRequestRouterPacket::write, SimpleRadioNetworking::handleRequestRouter);
+        registry.register(ServerboundRadioUpdatePacket.TYPE, ServerboundRadioUpdatePacket.class, ServerboundRadioUpdatePacket.STREAM_CODEC, SimpleRadioNetworking::handleRadioUpdate);
+        registry.register(ServerboundRequestRouterPacket.TYPE, ServerboundRequestRouterPacket.class, ServerboundRequestRouterPacket.STREAM_CODEC, SimpleRadioNetworking::handleRequestRouter);
     }
 
     public static void loadClientbound(ClientboundRegistry registry) {
-        registry.register(ClientboundActivityPacket.ID, ClientboundActivityPacket.class, ClientboundActivityPacket::read, ClientboundActivityPacket::write, SimpleRadioClientNetworking::handleActivityPacket);
-        registry.register(ClientboundRegisterRouterPacket.ID, ClientboundRegisterRouterPacket.class, ClientboundRegisterRouterPacket::read, ClientboundRegisterRouterPacket::write, SimpleRadioClientNetworking::handleRegisterRouter);
-        registry.register(ClientboundSpeakSoundPacket.ID, ClientboundSpeakSoundPacket.class, ClientboundSpeakSoundPacket::read, ClientboundSpeakSoundPacket::write, SimpleRadioClientNetworking::handleSpeakSound);
-        registry.register(ClientboundWireEffectPacket.ID, ClientboundWireEffectPacket.class, ClientboundWireEffectPacket::read, ClientboundWireEffectPacket::write, SimpleRadioClientNetworking::handleWireEffect);
+        registry.register(ClientboundActivityPacket.TYPE, ClientboundActivityPacket.class, ClientboundActivityPacket.STREAM_CODEC, SimpleRadioClientNetworking::handleActivityPacket);
+        registry.register(ClientboundRegisterRouterPacket.TYPE, ClientboundRegisterRouterPacket.class, ClientboundRegisterRouterPacket.STREAM_CODEC, SimpleRadioClientNetworking::handleRegisterRouter);
+        registry.register(ClientboundSpeakSoundPacket.TYPE, ClientboundSpeakSoundPacket.class, ClientboundSpeakSoundPacket.STREAM_CODEC, SimpleRadioClientNetworking::handleSpeakSound);
+        registry.register(ClientboundWireEffectPacket.TYPE, ClientboundWireEffectPacket.class, ClientboundWireEffectPacket.STREAM_CODEC, SimpleRadioClientNetworking::handleWireEffect);
     }
 
     // ---- Handlers ---- \\
 
     public static void handleRadioUpdate(ServerboundRadioUpdatePacket packet, MinecraftServer server, ServerPlayer player) {
         server.execute(() -> {
-            if (!RadioManager.getInstance().frequencies().check(packet.frequency())) return;
+            if (!Frequency.check(packet.frequency())) return;
 
             AbstractContainerMenu menu = player.containerMenu;
             if (menu instanceof RadiosmitherMenu radiosmitherMenu) {
@@ -69,11 +68,11 @@ public class SimpleRadioNetworking {
 
     public static void handleRequestRouter(ServerboundRequestRouterPacket packet, MinecraftServer server, ServerPlayer player) {
         UUID reference = packet.reference();
-        String type = packet.type();
+        String type = packet.routerType();
         short mapping = packet.mapping();
 
         server.execute(() -> {
-            short identifier = RadioManager.getInstance().getIdentifier(router -> reference.equals(router.getReference()) && router.getClass().getSimpleName().equals(type));
+            short identifier = RadioManager.getIdentifier(r -> reference.equals(r.reference) && r.getClass().getSimpleName().equals(type));
             if (identifier == Short.MAX_VALUE) {
                 CommonSimpleRadio.warn("We could not find the {} with reference {} for mapping {}!", type, reference, mapping);
                 return;
