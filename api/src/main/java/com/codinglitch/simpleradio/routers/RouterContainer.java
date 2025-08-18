579be3efce1f5afd39e17ab2802b2a88efd44802
CodinGlitch <codinglitchdev@gmail.com>
2025-08-18 04:11:51 -0700

    Made RouterContainer properly use reference so receiver/transmitter routers can sync

--- api/src/main/java/com/codinglitch/simpleradio/routers/RouterContainer.java
+++ api/src/main/java/com/codinglitch/simpleradio/routers/RouterContainer.java
@@ -1,21 +1,34 @@
 package com.codinglitch.simpleradio.routers;
 
 import com.codinglitch.simpleradio.ServerSimpleRadioApi;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.function.Predicate;
 
-public class RouterContainer<R extends Router> extends ArrayList<R> {
-    @Override
+public class RouterContainer<R extends Router> extends AbstractList<R> {
+    private final ArrayList<R> content = new ArrayList<>();
+
     public boolean add(R router) {
         ServerSimpleRadioApi.getInstance().pushRouter(router);
 
-        return super.add(router);
+        return content.add(router);
     }
 
     @Override
+    public R get(int index) {
+        return content.get(index);
+    }
+
+    @Override
+    public R remove(int index) {
+        return content.remove(index);
+    }
+
     public boolean removeIf(Predicate<? super R> filter) {
-        return super.removeIf(router -> {
+        return content.removeIf(router -> {
             if (router == null) return true;
 
             if (filter.test(router)) {
@@ -24,5 +37,20 @@ public class RouterContainer<R extends Router> extends ArrayList<R> {
             }
             return false;
         });
+    }
+
+    public ArrayList<R> getContent() {
+        return content;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<R> iterator() {
+        return content.iterator();
+    }
+
+    @Override
+    public int size() {
+        return content.size();
     }
 }
