9f5e2cfc10c37103861a60e9e85ab638cc509063
CodinGlitch <codinglitchdev@gmail.com>
2025-09-01 13:29:33 -0700

    Additional checks for feedback listening

--- common/src/main/java/com/codinglitch/simpleradio/radio/RadioManager.java
+++ common/src/main/java/com/codinglitch/simpleradio/radio/RadioManager.java
@@ -465,7 +465,18 @@ public class RadioManager extends ServerSimpleRadioApi {
 
     // I mixin here instead of using the appropriate events to access the channel as well as prevent duplicates
     public void onLocationalPacket(Level level, LocationalAudioChannel channel, byte[] data) {
-        if (!SimpleRadioLibrary.SERVER_CONFIG.router.feedbackListening) return;
+
+        String category = channel.getCategory();
+        if (category != null) {
+            if (category.equals("speakers")) {
+                if (!SimpleRadioLibrary.SERVER_CONFIG.router.feedbackListening) return;
+            } else {
+                if (category.equals("music_discs") || category.equals("note_blocks") || category.equals("goat_horns"))
+                    if (!SimpleRadioLibrary.SERVER_CONFIG.compatibilities.audioplayer.enabled) return;
+            }
+        }
+
+
         Vector3f senderPosition = new Vector3f((float) channel.getLocation().getX(), (float) channel.getLocation().getY(), (float) channel.getLocation().getZ());
         sendAudio(WorldlyPosition.of(senderPosition, level), channel.getId(), data);
     }
