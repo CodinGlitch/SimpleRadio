48db7cc8f24e5399364aeaaf4faec3730ae6cf7f
CodinGlitch <codinglitchdev@gmail.com>
2025-11-09 12:33:14 -0800

    Crude album jukebox support and temporary "dual-download" patch

--- fabric/src/main/java/com/codinglitch/simpleradio/platform/FabricClientCompatPlatform.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/platform/FabricClientCompatPlatform.java
@@ -2,19 +2,19 @@ package com.codinglitch.simpleradio.platform;
 
 import com.codinglitch.simpleradio.platform.services.ClientCompatPlatform;
 import com.codinglitch.simpleradio.routers.Router;
-import net.minecraft.client.resources.sounds.AbstractSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.AudioStream;
 
 import java.util.concurrent.CompletableFuture;
 
 public class FabricClientCompatPlatform implements ClientCompatPlatform {
     @Override
-    public CompletableFuture<AudioStream> makeSubstream(AbstractSoundInstance sound) {
+    public CompletableFuture<AudioStream> makeSubstream(SoundInstance sound) {
         return ClientCompatPlatform.super.makeSubstream(sound);
     }
 
     @Override
-    public AbstractSoundInstance makeSound(Router router, String soundString, long seed) {
+    public SoundInstance makeSound(Router router, String soundString, float volume, float pitch, float severity, float offset, long seed) {
         return null;
     }
 }
