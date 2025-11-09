fce50883cc0e9e1272b93bd209a3748483bd5e38
CodinGlitch <codinglitchdev@gmail.com>
2025-11-09 12:32:14 -0800

    Separated client and server compat for etched

--- forge/src/main/java/com/codinglitch/simpleradio/platform/ForgeClientCompatPlatform.java
+++ forge/src/main/java/com/codinglitch/simpleradio/platform/ForgeClientCompatPlatform.java
@@ -1,20 +1,21 @@
 package com.codinglitch.simpleradio.platform;
 
 import com.codinglitch.simpleradio.CompatCore;
-import com.codinglitch.simpleradio.compat.EtchedCompat;
+import com.codinglitch.simpleradio.compat.etched.ClientEtchedCompat;
+import com.codinglitch.simpleradio.compat.etched.EtchedCompat;
 import com.codinglitch.simpleradio.platform.services.ClientCompatPlatform;
 import com.codinglitch.simpleradio.routers.Router;
-import net.minecraft.client.resources.sounds.AbstractSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.AudioStream;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ForgeClientCompatPlatform implements ClientCompatPlatform {
     @Override
-    public CompletableFuture<AudioStream> makeSubstream(AbstractSoundInstance sound) {
+    public CompletableFuture<AudioStream> makeSubstream(SoundInstance sound) {
         // ---- Etched ---- \\
         if (CompatCore.ETCHED.enabled) {
-            CompletableFuture<AudioStream> result = EtchedCompat.makeSubstream(sound);
+            CompletableFuture<AudioStream> result = ClientEtchedCompat.makeSubstream(sound);
             if (result != null) return result;
         }
 
@@ -22,10 +23,10 @@ public class ForgeClientCompatPlatform implements ClientCompatPlatform {
     }
 
     @Override
-    public AbstractSoundInstance makeSound(Router router, String soundString, long seed) {
+    public SoundInstance makeSound(Router router, String soundString, float volume, float pitch, float severity, float offset, long seed) {
         // ---- Etched ---- \\
         if (CompatCore.ETCHED.enabled) {
-            AbstractSoundInstance result = EtchedCompat.makeSound(router, soundString, seed);
+            SoundInstance result = ClientEtchedCompat.makeSound(router, soundString, volume, pitch, severity, offset, seed);
             if (result != null) return result;
         }
 
