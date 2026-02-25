b36ce477d4cd21ed8f4b2d8aec337ab6b88b54cd
CodinGlitch <codinglitchdev@gmail.com>
2026-02-25 05:38:12 -0800

    Use register method

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioComponents.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/SimpleRadioComponents.java
@@ -2,9 +2,7 @@ package com.codinglitch.simpleradio.core.registry;
 
 import com.codinglitch.simpleradio.CommonSimpleRadio;
 import com.mojang.serialization.Codec;
-import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.LinkedHashMap;
@@ -13,32 +11,27 @@ import java.util.Map;
 public class SimpleRadioComponents {
     public static final Map<ResourceLocation, DataComponentType<?>> COMPONENT_TYPES = new LinkedHashMap<>();
 
-    public static final DataComponentType<String> FREQUENCY = Registry.register(
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
+    public static final DataComponentType<String> FREQUENCY = register(
             CommonSimpleRadio.id("frequency"),
             DataComponentType.<String>builder().persistent(Codec.STRING).build()
     );
 
-    public static final DataComponentType<String> MODULATION = Registry.register(
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
+    public static final DataComponentType<String> MODULATION = register(
             CommonSimpleRadio.id("modulation"),
             DataComponentType.<String>builder().persistent(Codec.STRING).build()
     );
 
-    public static final DataComponentType<String> REFERENCE = Registry.register(
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
+    public static final DataComponentType<String> REFERENCE = register(
             CommonSimpleRadio.id("reference"),
             DataComponentType.<String>builder().persistent(Codec.STRING).build()
     );
 
-    public static final DataComponentType<Boolean> ACTIVATED = Registry.register(
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
+    public static final DataComponentType<Boolean> ACTIVATED = register(
             CommonSimpleRadio.id("activated"),
             DataComponentType.<Boolean>builder().persistent(Codec.BOOL).build()
     );
 
-    public static final DataComponentType<String> MODULE = Registry.register(
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
+    public static final DataComponentType<String> MODULE = register(
             CommonSimpleRadio.id("module"),
             DataComponentType.<String>builder().persistent(Codec.STRING).build()
     );
