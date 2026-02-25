04ff924bce237754c79293a2a019f5e485b54908
CodinGlitch <codinglitchdev@gmail.com>
2026-02-25 05:27:25 -0800

    Remove unnecessary overrides

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/items/SpeakerItem.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/items/SpeakerItem.java
@@ -5,34 +5,13 @@ import com.codinglitch.simpleradio.central.Speaking;
 import com.codinglitch.simpleradio.core.central.Alterable;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioBlocks;
 import com.codinglitch.simpleradio.core.registry.SimpleRadioModules;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class SpeakerItem extends BlockItem implements Speaking, Alterable {
     public SpeakerItem(Properties settings) {
         super(SimpleRadioBlocks.SPEAKER, settings);
-    }
-
-    @Override
-    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag tooltip) {
-        //appendTooltip(stack, components);
-        super.appendHoverText(stack, level, components, tooltip);
-    }
-
-    @Override
-    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean b) {
-        super.inventoryTick(stack, level, entity, slot, b);
-
-        //tick(stack, level);
     }
 
     @Override
