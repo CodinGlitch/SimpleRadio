fdef89a512ea16143221586ae9faf3d504ab8faf
CodinGlitch <codinglitchdev@gmail.com>
2026-03-27 18:43:41 -0700

    [Common] More game tests

--- common/src/main/java/com/codinglitch/simpleradio/test/RadioTests.java
+++ common/src/main/java/com/codinglitch/simpleradio/test/RadioTests.java
@@ -2,29 +2,47 @@ package com.codinglitch.simpleradio.test;
 
 import com.codinglitch.simpleradio.ServerSimpleRadioApi;
 import com.codinglitch.simpleradio.SimpleRadioApi;
+import com.codinglitch.simpleradio.central.WorldlyPosition;
 import com.codinglitch.simpleradio.routers.Router;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTestAssertException;
 import net.minecraft.gametest.framework.GameTestAssertPosException;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
 import java.util.Optional;
 
 public class RadioTests {
     public static <T extends Router> T assertRouter(final GameTestHelper helper, Class<T> type, BlockPos pos) {
-        Optional<Router> router = ServerSimpleRadioApi.getInstance().getRouters()
-                .stream().filter(r -> helper.relativePos(r.getLocation().blockPos()).equals(pos))
-                .findFirst();
+        Optional<Router> router =
+                ServerSimpleRadioApi.getInstance().getRouters().stream().filter(r ->
+                        relativePos(helper, r.getLocation()).equals(pos) && type.isAssignableFrom(r.getClass())
+                ).findFirst();
 
         if (router.isEmpty()) {
-            throw new GameTestAssertPosException("Router not found at location", helper.absolutePos(pos), pos, helper.getTick());
-        }
-
-        if (router.get().getClass().isAssignableFrom(type)) {
-            throw new GameTestAssertPosException("Router is incorrect type "+type.getName()+" != "+router.get().getClass().getName(), helper.absolutePos(pos), pos, helper.getTick());
+            throw new GameTestAssertPosException(type.getSimpleName()+" not found at location", helper.absolutePos(pos), pos, helper.getTick());
         }
 
         return (T) router.get();
+    }
+
+    public static <T extends Router> void assertNoRouter(final GameTestHelper helper, Class<T> type, BlockPos pos) {
+        Optional<Router> router =
+                ServerSimpleRadioApi.getInstance().getRouters().stream().filter(r ->
+                        relativePos(helper, r.getLocation()).equals(pos) && type.isAssignableFrom(r.getClass())
+                ).findFirst();
+
+        if (router.isPresent()) {
+            throw new GameTestAssertPosException(type.getSimpleName()+" found at location", helper.absolutePos(pos), pos, helper.getTick());
+        }
+    }
+
+    public static BlockPos relativePos(final GameTestHelper helper, WorldlyPosition pos) {
+        BlockPos blockpos = helper.absolutePos(BlockPos.ZERO);
+        BlockPos blockpos1 = StructureTemplate.transform(pos.blockPos(), Mirror.NONE, helper.getTestRotation(), blockpos);
+        return blockpos1.subtract(blockpos);
     }
 }
--- common/src/main/java/com/codinglitch/simpleradio/test/SystemTest.java
+++ common/src/main/java/com/codinglitch/simpleradio/test/SystemTest.java
@@ -2,42 +2,186 @@ package com.codinglitch.simpleradio.test;
 
 import com.codinglitch.simpleradio.ServerSimpleRadioApi;
 import com.codinglitch.simpleradio.central.WorldlyPosition;
+import com.codinglitch.simpleradio.core.registry.SimpleRadioEntities;
 import com.codinglitch.simpleradio.radio.RadioListener;
 import com.codinglitch.simpleradio.radio.RadioManager;
+import com.codinglitch.simpleradio.radio.RadioRouter;
 import com.codinglitch.simpleradio.radio.RadioSpeaker;
-import com.codinglitch.simpleradio.routers.Listener;
-import com.codinglitch.simpleradio.routers.Speaker;
+import com.codinglitch.simpleradio.routers.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.phys.Vec3;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SystemTest {
 
+    @GameTest(template = "simpleradio:system/all_routers")
+    public void testRouterRegistry(final GameTestHelper helper) {
+        helper.runAtTickTime(5, () -> {
+            RadioTests.assertRouter(helper, Speaker.class, new BlockPos(2, 2, 2));
+            RadioTests.assertRouter(helper, Listener.class, new BlockPos(1, 2, 2));
+            RadioTests.assertRouter(helper, Router.class, new BlockPos(0, 2, 2));
+
+            RadioTests.assertRouter(helper, Receiver.class, new BlockPos(2, 2, 1));
+            RadioTests.assertRouter(helper, Speaker.class, new BlockPos(2, 2, 1));
+
+            RadioTests.assertRouter(helper, Transmitter.class, new BlockPos(1, 2, 1));
+            RadioTests.assertRouter(helper, Receiver.class, new BlockPos(0, 2, 1));
+
+            RadioTests.assertRouter(helper, Listener.class, new BlockPos(2, 2, 0));
+            RadioTests.assertRouter(helper, Speaker.class, new BlockPos(2, 2, 0));
+            RadioTests.assertRouter(helper, Receiver.class, new BlockPos(2, 2, 0));
+            RadioTests.assertRouter(helper, Transmitter.class, new BlockPos(2, 2, 0));
+
+            helper.succeed();
+        });
+    }
+
+    @GameTest(template = "simpleradio:system/all_routers")
+    public void testGarbageCollection(final GameTestHelper helper) {
+        helper.runAtTickTime(20, () -> {
+            helper.destroyBlock(new BlockPos(2, 2, 2));
+            helper.destroyBlock(new BlockPos(1, 2, 2));
+            helper.destroyBlock(new BlockPos(0, 2, 2));
+            helper.destroyBlock(new BlockPos(2, 2, 1));
+            helper.destroyBlock(new BlockPos(1, 2, 1));
+            helper.destroyBlock(new BlockPos(0, 2, 1));
+
+            helper.killAllEntities();
+        });
+
+        helper.runAtTickTime(50, () -> {
+            RadioTests.assertNoRouter(helper, Speaker.class, new BlockPos(2, 2, 2));
+            RadioTests.assertNoRouter(helper, Listener.class, new BlockPos(1, 2, 2));
+            RadioTests.assertNoRouter(helper, Router.class, new BlockPos(0, 2, 2));
+
+            RadioTests.assertNoRouter(helper, Receiver.class, new BlockPos(2, 2, 1));
+            RadioTests.assertNoRouter(helper, Speaker.class, new BlockPos(2, 2, 1));
+
+            RadioTests.assertNoRouter(helper, Transmitter.class, new BlockPos(1, 2, 1));
+            RadioTests.assertNoRouter(helper, Receiver.class, new BlockPos(0, 2, 1));
+
+            RadioTests.assertNoRouter(helper, Listener.class, new BlockPos(2, 2, 0));
+            RadioTests.assertNoRouter(helper, Speaker.class, new BlockPos(2, 2, 0));
+            RadioTests.assertNoRouter(helper, Receiver.class, new BlockPos(2, 2, 0));
+            RadioTests.assertNoRouter(helper, Transmitter.class, new BlockPos(2, 2, 0));
+
+            helper.succeed();
+        });
+    }
+
     @GameTest(template = "simpleradio:system/closed_system")
     public void testClosedSystem(final GameTestHelper helper) {
-        helper.runAtTickTime(5, () -> {
-            RadioTests.assertRouter(helper, Listener.class, new BlockPos(-1, 2, -1));
-            RadioSpeaker speaker = (RadioSpeaker) RadioTests.assertRouter(helper, Speaker.class, new BlockPos(-3, 2, -3));
+        AtomicBoolean received = new AtomicBoolean(false);
 
-            AtomicBoolean received = new AtomicBoolean(false);
+        helper.runAfterDelay(10, () -> {
+            RadioTests.assertRouter(helper, Listener.class, new BlockPos(0, 2, 0));
+            RadioSpeaker speaker = (RadioSpeaker) RadioTests.assertRouter(helper, Speaker.class, new BlockPos(2, 2, 2));
+
             speaker.acceptCriteria = source -> {
                 received.set(true);
                 return true;
             };
 
-            ServerSimpleRadioApi.getInstance().sendSound(
+            ServerSimpleRadioApi.getInstance().sendAudio(
                     WorldlyPosition.of(
-                            helper.absolutePos(new BlockPos(-1, 2, -1)),
+                            helper.absolutePos(new BlockPos(0, 2, 0)),
                             helper.getLevel()
                     ),
-                    SoundEvents.ALLAY_AMBIENT_WITH_ITEM,
-                    1, 1, 1
+                    UUID.randomUUID(),
+                    new byte[] {}
             );
 
-            helper.succeedWhen(received::get);
+        });
+
+        helper.onEachTick(() -> {
+            if (received.get()) helper.succeed();
+        });
+    }
+
+    @GameTest(template = "simpleradio:system/wire_survival")
+    public void testWireSurvival(final GameTestHelper helper) {
+        helper.runAfterDelay(10, () -> {
+            helper.assertEntityPresent(SimpleRadioEntities.WIRE, new BlockPos(1, 1, 1), 4);
+            helper.succeed();
+        });
+    }
+
+    @GameTest(template = "simpleradio:system/open_system")
+    public void testOpenSystem(final GameTestHelper helper) {
+        AtomicBoolean received = new AtomicBoolean(false);
+
+        helper.runAfterDelay(10, () -> {
+            RadioTests.assertRouter(helper, Listener.class, new BlockPos(0, 2, 0));
+            RadioSpeaker speaker = (RadioSpeaker) RadioTests.assertRouter(helper, Speaker.class, new BlockPos(2, 2, 0));
+
+            speaker.acceptCriteria = source -> {
+                received.set(true);
+                return true;
+            };
+
+            ServerSimpleRadioApi.getInstance().sendAudio(
+                    WorldlyPosition.of(
+                            helper.absolutePos(new BlockPos(0, 2, 0)),
+                            helper.getLevel()
+                    ),
+                    UUID.randomUUID(),
+                    new byte[] {}
+            );
+
+        });
+
+        helper.onEachTick(() -> {
+            if (received.get()) helper.succeed();
+        });
+    }
+
+    @GameTest(template = "simpleradio:system/short_circuit")
+    public void testShortCircuit(final GameTestHelper helper) {
+        helper.runAfterDelay(10, () -> {
+            Router router = RadioTests.assertRouter(helper, Router.class, new BlockPos(0, 2, 1));
+
+            router.send(new byte[] {}, 0);
+
+            helper.runAfterDelay(40, () -> {
+                helper.assertEntityNotPresent(SimpleRadioEntities.WIRE,
+                        helper.absoluteVec(new Vec3(0, 1, 1)),
+                        helper.absoluteVec(new Vec3(1, 3, 2))
+                );
+                helper.succeed();
+            });
+        });
+    }
+
+    @GameTest(template = "simpleradio:system/transceiver_ground_to_ground")
+    public void testTransceiverGroundToGround(final GameTestHelper helper) {
+        AtomicBoolean received = new AtomicBoolean(false);
+
+        helper.runAtTickTime(5, () -> {
+            RadioSpeaker speaker = (RadioSpeaker) RadioTests.assertRouter(helper, Speaker.class, new BlockPos(4, 2, 0));
+            speaker.acceptCriteria = source -> {
+                received.set(true);
+                return true;
+            };
+
+            ServerSimpleRadioApi.getInstance().sendAudio(
+                    WorldlyPosition.of(
+                            helper.absolutePos(new BlockPos(1, 2, 0)),
+                            helper.getLevel()
+                    ),
+                    UUID.randomUUID(),
+                    new byte[] {}
+            );
+        });
+
+        helper.onEachTick(() -> {
+            if (received.get()) helper.succeed();
         });
     }
 }
