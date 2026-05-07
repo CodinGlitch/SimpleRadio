0f519e596afcdb62fe2356f1e32fe64232e71418
CodinGlitch <codinglitchdev@gmail.com>
2026-05-06 20:21:48 -0700

    Frame-time router positioning

--- neoforge/src/main/java/com/codinglitch/simpleradio/platform/NeoForgeClientCompatPlatform.java
+++ neoforge/src/main/java/com/codinglitch/simpleradio/platform/NeoForgeClientCompatPlatform.java
@@ -1,9 +1,12 @@
 package com.codinglitch.simpleradio.platform;
 
+import com.codinglitch.simpleradio.CompatCore;
+import com.codinglitch.simpleradio.central.WorldlyPosition;
 import com.codinglitch.simpleradio.platform.services.ClientCompatPlatform;
 import com.codinglitch.simpleradio.routers.Router;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.AudioStream;
+import org.joml.Quaternionf;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -16,5 +19,25 @@ public class NeoForgeClientCompatPlatform implements ClientCompatPlatform {
     @Override
     public SoundInstance makeSound(Router router, String soundString, float volume, float pitch, float severity, float offset, long seed) {
         return null;
+    }
+
+    @Override
+    public WorldlyPosition modifyPosition(WorldlyPosition position) {
+
+        // ---- Valkyrien Skies ---- \\
+        if (CompatCore.VALKYRIEN_SKIES.enabled) {
+        }
+
+        return position;
+    }
+
+    @Override
+    public Quaternionf modifyRotation(WorldlyPosition position, Quaternionf rotation) {
+
+        // ---- Valkyrien Skies ---- \\
+        if (CompatCore.VALKYRIEN_SKIES.enabled) {
+        }
+
+        return rotation;
     }
 }
