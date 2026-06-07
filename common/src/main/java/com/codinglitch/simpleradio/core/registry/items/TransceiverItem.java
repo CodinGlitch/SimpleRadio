8f00cbe6c6e066857158d9c64acdf8e3e46bc536
CodinGlitch <codinglitchdev@gmail.com>
2026-06-06 18:41:22 -0700

    Fix lexiconfig versioning

--- common/src/main/java/com/codinglitch/simpleradio/core/registry/items/TransceiverItem.java
+++ common/src/main/java/com/codinglitch/simpleradio/core/registry/items/TransceiverItem.java
@@ -25,8 +25,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.UUID;
 
