d262fd6314ad19dc6010a6c8af9166a8fc923cb3
CodinGlitch <codinglitchdev@gmail.com>
2026-06-13 16:56:22 -0700

    Fixes

--- common/src/main/java/com/codinglitch/simpleradio/datagen/CommonBlockTagProvider.java
+++ common/src/main/java/com/codinglitch/simpleradio/datagen/CommonBlockTagProvider.java
@@ -4,18 +4,13 @@ import com.codinglitch.simpleradio.core.registry.SimpleRadioBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class CommonBlockTagProvider extends TagsProvider<Block> {
