9d78319a34cb4f4314f5172f89c2c5a9900e7015
CodinGlitch <codinglitchdev@gmail.com>
2026-06-13 16:11:51 -0700

    [Fabric] Add handheld PTT enabling

---
+++ fabric/src/main/java/com/codinglitch/simpleradio/mixin/MixinPTTMicrophoneProcessor.java
@@ -0,0 +1,21 @@
+package com.codinglitch.simpleradio.mixin;
+
+import com.codinglitch.simpleradio.client.ClientRadioManager;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import de.maxhenkel.voicechat.voice.client.PTTMicrophoneProcessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PTTMicrophoneProcessor.class)
+public abstract class MixinPTTMicrophoneProcessor {
+
+    @WrapOperation(
+            method = "processInternal",
+            at = @At(value = "INVOKE", target = "Lde/maxhenkel/voicechat/voice/client/PTTMicrophoneProcessor;isPttButtonDown()Z"),
+            remap = false
+    )
+    private boolean simpleradio$processInternal_handheldPTT(PTTMicrophoneProcessor instance, Operation<Boolean> original) {
+        return ClientRadioManager.isUsingHandheld() || original.call(instance);
+    }
+}
