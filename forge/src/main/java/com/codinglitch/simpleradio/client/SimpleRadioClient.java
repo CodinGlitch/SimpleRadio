8e0b30ffbb171100053aee0c9bb1ae0dbec44b15
CodinGlitch <codinglitchdev@gmail.com>
2026-06-16 10:27:17 -0700

    [Forge] Key bindings

--- forge/src/main/java/com/codinglitch/simpleradio/client/SimpleRadioClient.java
+++ forge/src/main/java/com/codinglitch/simpleradio/client/SimpleRadioClient.java
@@ -6,7 +6,10 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,11 +32,28 @@ public class SimpleRadioClient {
         CommonSimpleRadioClient.loadParticles(event::registerSpriteSet);
     }
 
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) { // Only call code once as the tick event is called twice every tick
+            for (Keybinds.Binding binding : Keybinds.BINDINGS) {
+                Keybinds.processBinding(binding);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerBindings(RegisterKeyMappingsEvent event) {
+        for (Keybinds.Binding binding : Keybinds.BINDINGS) {
+            event.register(binding.mapping);
+        }
+    }
+
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         CommonSimpleRadioClient.initialize();
         CommonSimpleRadioClient.loadProperties(ItemProperties::register);
 
         event.enqueueWork(CommonSimpleRadioClient::loadScreens);
+
+        MinecraftForge.EVENT_BUS.addListener(SimpleRadioClient::onClientTick);
     }
 }
