fa5acdb48ff4f5b6c3864f5181ac2233b96fd12e
CodinGlitch <codinglitchdev@gmail.com>
2026-06-16 10:27:13 -0700

    [Fabric] Key bindings

--- fabric/src/main/java/com/codinglitch/simpleradio/client/SimpleRadioClient.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/client/SimpleRadioClient.java
@@ -3,10 +3,13 @@ package com.codinglitch.simpleradio.client;
 import com.codinglitch.simpleradio.core.FabricLoader;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.impl.client.particle.ParticleFactoryRegistryImpl;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.SpriteSet;
@@ -34,6 +37,15 @@ public class SimpleRadioClient implements ClientModInitializer {
             @Override
             public <O extends ParticleOptions> void register(ParticleType<O> type, ParticleEngine.SpriteParticleRegistration<O> registration) {
                 ParticleFactoryRegistry.getInstance().register(type, registration::create);
+            }
+        });
+
+        for (Keybinds.Binding binding : Keybinds.BINDINGS) {
+            KeyBindingHelper.registerKeyBinding(binding.mapping);
+        }
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            for (Keybinds.Binding binding : Keybinds.BINDINGS) {
+                Keybinds.processBinding(binding);
             }
         });
 
