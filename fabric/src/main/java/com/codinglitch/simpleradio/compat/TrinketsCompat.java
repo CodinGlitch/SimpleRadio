fc4f46f4d0b1bf9b77889230b03621bec438a6dd
CodinGlitch <codinglitchdev@gmail.com>
2026-06-17 11:24:40 -0700

    [Fabric] Update trinkets compat

--- fabric/src/main/java/com/codinglitch/simpleradio/compat/TrinketsCompat.java
+++ fabric/src/main/java/com/codinglitch/simpleradio/compat/TrinketsCompat.java
@@ -3,9 +3,16 @@ package com.codinglitch.simpleradio.compat;
 import com.codinglitch.simpleradio.CommonSimpleRadio;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class TrinketsCompat {
 
@@ -30,5 +37,17 @@ public class TrinketsCompat {
             });
 
         }));
+    }
+
+    public static ItemStack getAccessory(Player player, Predicate<ItemStack> filter) {
+        Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(player);
+        if (component.isEmpty()) return ItemStack.EMPTY;
+
+        List<Tuple<SlotReference, ItemStack>> result = component.get().getEquipped(filter);
+        if (result.isEmpty()) return ItemStack.EMPTY;
+
+        return result.stream()
+                .findFirst()
+                .get().getB();
     }
 }
