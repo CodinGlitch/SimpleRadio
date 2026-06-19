10825d50f77b111f9fb59e77d4cf2b56da0f9949
CodinGlitch <codinglitchdev@gmail.com>
2026-06-18 20:30:29 -0700

    [Common] Change accessory rendering

--- common/src/main/java/com/codinglitch/simpleradio/compat/AccessoryCompat.java
+++ common/src/main/java/com/codinglitch/simpleradio/compat/AccessoryCompat.java
@@ -32,7 +32,7 @@ public class AccessoryCompat {
         void register(Item accessory, BiConsumer<Context, ItemStack> ticker);
     }
 
-    public static void makeItems(AccessoryRegistry registry) {
+    public static void makeItems(AccessoryRegistry registry, BiConsumer<Item, AccessoryRenderer> rendererProvider) {
         BiConsumer<Context, ItemStack> handheldTicker = (context, stack) -> {
             if (!context.identifier().equals("trinket")) return;
 
@@ -45,6 +45,13 @@ public class AccessoryCompat {
         registry.register(SimpleRadioItems.TRANSCEIVER, handheldTicker);
         registry.register(SimpleRadioItems.WALKIE_TALKIE, handheldTicker);
         registry.register(SimpleRadioItems.SPUDDIE_TALKIE, handheldTicker);
+
+        Services.PLATFORM.forClient(() -> {
+            rendererProvider.accept(SimpleRadioItems.TRANSCEIVER, new HandheldRenderer());
+            rendererProvider.accept(SimpleRadioItems.WALKIE_TALKIE, new HandheldRenderer());
+            rendererProvider.accept(SimpleRadioItems.SPUDDIE_TALKIE, new HandheldRenderer());
+        });
+
     }
 
     public static ItemStack getAccessory(Player player, Predicate<ItemStack> filter) {
@@ -66,8 +73,20 @@ public class AccessoryCompat {
         }
     }
 
-    public static class HandheldRenderer {
-        public static <T extends LivingEntity, M extends EntityModel<T>> void render(
+    public abstract static class AccessoryRenderer {
+        public abstract <T extends LivingEntity, M extends EntityModel<T>> void render(
+                ItemStack stack,
+                LivingEntity livingEntity,
+                PoseStack poseStack,
+                MultiBufferSource buffer,
+                int light
+        );
+    }
+
+    public static class HandheldRenderer extends AccessoryRenderer {
+
+        @Override
+        public <T extends LivingEntity, M extends EntityModel<T>> void render(
                 ItemStack stack,
                 LivingEntity livingEntity,
                 PoseStack poseStack,
@@ -97,6 +116,5 @@ public class AccessoryCompat {
 
             poseStack.popPose();
         }
-
     }
 }
